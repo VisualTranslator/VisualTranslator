a2dcc7494f42076102676e9ef0906acddaf50643
Andriy Semenets <semanser@gmail.com>
2017-04-29 22:54:35 +0300

    Added "Translate to" & "Translate from" submenus

--- tray.cpp
+++ tray.cpp
@@ -11,11 +11,53 @@ Tray::Tray(QWidget *parent) : QWidget(parent)
 void Tray::generateMenu()
 {
     menu = new QMenu(this);
+    langFromMenu = new QMenu("Translate from", menu);
+    langToMenu = new QMenu("Translate to", menu);
+    menuLangFromGroup = new QActionGroup(this);
+    menuLangToGroup = new QActionGroup(this);
+
+    addLangMenuItem("from", "Autodetect", QVariant("auto"));
+    addLangMenuItem("to", "Ukrainian", QVariant("ua"), QIcon(":/lang-icons/ua.png"));
+
+    menu->addMenu(langFromMenu);
+    menu->addMenu(langToMenu);
+    menu->addSeparator();
     menu->addAction("Exit", qApp, SLOT(quit()));
+
     trayIcon->setContextMenu(menu);
 }
 
 void Tray::showMessage()
 {
      trayIcon->showMessage(QString("Message"), QString("Test message"));
+}
+
+void Tray::chooseFromLang()
+{
+    QAction *action = qobject_cast<QAction*>(sender());
+    qDebug()<<action->data().toString();
+}
+
+void Tray::chooseToLang()
+{
+    QAction *action = qobject_cast<QAction*>(sender());
+    qDebug()<<action->data().toString();
+}
+
+void Tray::addLangMenuItem(QString type, QString title, QVariant data , QIcon icon)
+{
+    QAction *item = new QAction(title);
+    item->setCheckable(true);
+    item->setData(data);
+    item->setIcon(icon);
+
+    if (type == "from") {
+       langFromMenu->addAction(item);
+       QObject::connect(item, SIGNAL(triggered()), this, SLOT(chooseFromLang()));
+       menuLangFromGroup->addAction(item);
+    } else {
+        langToMenu->addAction(item);
+        QObject::connect(item, SIGNAL(triggered()), this, SLOT(chooseToLang()));
+        menuLangToGroup->addAction(item);
+    }
 }
--- tray.h
+++ tray.h
@@ -7,15 +7,22 @@ class Tray : public QWidget
 {
     Q_OBJECT
 private:
+    QActionGroup *menuLangToGroup;
+    QActionGroup *menuLangFromGroup;
     virtual void generateMenu();
+    void addLangMenuItem(QString type, QString title, QVariant data , QIcon icon = QIcon());
 public:
     explicit Tray(QWidget *parent = 0);
     void showMessage();
     QSystemTrayIcon *trayIcon;
     QMenu *menu;
+    QMenu *langToMenu;
+    QMenu *langFromMenu;
 signals:
 
 public slots:
+    void chooseFromLang();
+    void chooseToLang();
 };
 
 #endif // TRAY_H
