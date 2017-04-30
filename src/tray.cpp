0261576ce14102b53c512c59d367224783ce79cd
Andriy Semenets <semanser@gmail.com>
2017-04-30 13:43:51 +0300

    Added new languages into the menu

--- src/tray.cpp
+++ src/tray.cpp
@@ -16,8 +16,30 @@ void Tray::generateMenu()
     menuLangFromGroup = new QActionGroup(this);
     menuLangToGroup = new QActionGroup(this);
 
-    addLangMenuItem("from", "Autodetect", QVariant("auto"));
-    addLangMenuItem("to", "Ukrainian", QVariant("ua"), QIcon(":/resources/lang_icons/ua.png"));
+    addLangMenuItem("from", "Autodetect", QVariant("auto"), QIcon(":/resources/lang_icons/auto.png"));
+    addLangMenuItem("from", "Arabic", QVariant("arabic"), QIcon(":/resources/lang_icons/sa.png"));
+    addLangMenuItem("from", "English", QVariant("spanish"), QIcon(":/resources/lang_icons/us.png"));
+    addLangMenuItem("from", "French", QVariant("french"), QIcon(":/resources/lang_icons/fr.png"));
+    addLangMenuItem("from", "German", QVariant("german"), QIcon(":/resources/lang_icons/de.png"));
+    addLangMenuItem("from", "Italian", QVariant("italian"), QIcon(":/resources/lang_icons/it.png"));
+    addLangMenuItem("from", "Japanese", QVariant("japanese"), QIcon(":/resources/lang_icons/jp.png"));
+    addLangMenuItem("from", "Korean", QVariant("korean"), QIcon(":/resources/lang_icons/kr.png"));
+    addLangMenuItem("from", "Portuguese", QVariant("portuguese"), QIcon(":/resources/lang_icons/pt.png"));
+    addLangMenuItem("from", "Russian", QVariant("russian"), QIcon(":/resources/lang_icons/ru.png"));
+    addLangMenuItem("from", "Spanish", QVariant("spanish"), QIcon(":/resources/lang_icons/es.png"));
+    addLangMenuItem("from", "Ukrainian", QVariant("ukrainian"), QIcon(":/resources/lang_icons/ua.png"));
+
+    addLangMenuItem("to", "Arabic", QVariant("arabic"), QIcon(":/resources/lang_icons/sa.png"));
+    addLangMenuItem("to", "English", QVariant("spanish"), QIcon(":/resources/lang_icons/us.png"));
+    addLangMenuItem("to", "French", QVariant("french"), QIcon(":/resources/lang_icons/fr.png"));
+    addLangMenuItem("to", "German", QVariant("german"), QIcon(":/resources/lang_icons/de.png"));
+    addLangMenuItem("to", "Italian", QVariant("italian"), QIcon(":/resources/lang_icons/it.png"));
+    addLangMenuItem("to", "Japanese", QVariant("japanese"), QIcon(":/resources/lang_icons/jp.png"));
+    addLangMenuItem("to", "Korean", QVariant("korean"), QIcon(":/resources/lang_icons/kr.png"));
+    addLangMenuItem("to", "Portuguese", QVariant("portuguese"), QIcon(":/resources/lang_icons/pt.png"));
+    addLangMenuItem("to", "Russian", QVariant("russian"), QIcon(":/resources/lang_icons/ru.png"));
+    addLangMenuItem("to", "Spanish", QVariant("spanish"), QIcon(":/resources/lang_icons/es.png"));
+    addLangMenuItem("to", "Ukrainian", QVariant("ukrainian"), QIcon(":/resources/lang_icons/ua.png"));
 
     menu->addMenu(langFromMenu);
     menu->addMenu(langToMenu);
