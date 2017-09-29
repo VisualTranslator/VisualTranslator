75e8bdf506ea92450a663b3cbb44e60b4fcd7246
Andriy Semenets <semanser@gmail.com>
2017-09-29 11:33:05 +0300

    Updated UI style

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,14 @@ int main(int argc, char *argv[])
 {
     App a(argc, argv, "Organization", "VisualTranslator");
     VisualTranslator *pVisualTranslator = new VisualTranslator;
+
+    QFile file(":/resources/stylesheet.qss");
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString style(file.readAll());
+        a.setStyleSheet(style);
+        file.close();
+    }
+
     return a.exec();
 }
