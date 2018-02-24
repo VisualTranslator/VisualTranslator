9ff2093adc87f3198c86655a111af778f8697a76
Andriy Semenets <semanser@gmail.com>
2018-02-24 13:40:13 +0200

    Clean main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,11 @@
 #include "visualtranslator.h"
 #include "app.h"
 
+// Entry point for the app
 int main(int argc, char *argv[])
 {
     App a(argc, argv, "Organization", "VisualTranslator");
     VisualTranslator *pVisualTranslator = new VisualTranslator;
-
-    QFile file(":/resources/stylesheet.qss");
-    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        QString style(file.readAll());
-        a.setStyleSheet(style);
-        file.close();
-    }
-
-    QFontDatabase::addApplicationFont(":/resources/fonts/OpenSans-Regular.ttf");
-    QFont OpenSans("OpenSans", 10, QFont::Normal);
-
-    a.setFont(OpenSans);
 
     return a.exec();
 }
--- src/visualtranslator.cpp
+++ src/visualtranslator.cpp
@@ -2,14 +2,30 @@
 
 VisualTranslator::VisualTranslator(QWidget *parent) : QWidget(parent)
 {
-   tray = new Tray;
-   recognizer = new Recognizer(this);
-   screenArea = new ScreenArea;
-   hotkey = new QHotkey(QKeySequence("ctrl+alt+Q"), true, this);
-   translator = new Translator;
+    // Setup the main stylesheet
+    QFile file(":/resources/stylesheet.qss");
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QString style(file.readAll());
+        this->setStyleSheet(style);
+        file.close();
+    }
 
-   QObject::connect(hotkey, SIGNAL(activated()), screenArea, SLOT(show()));
-   QObject::connect(screenArea, SIGNAL(selected(QPixmap)), recognizer, SLOT(start(QPixmap)));
-   QObject::connect(recognizer, SIGNAL(recognized(QString)), translator, SLOT(translate(QString)));
-   QObject::connect(translator, SIGNAL(translated(QString, QString)), tray, SLOT(showMessage(QString,QString)));
+    // Setup the main font
+    QFontDatabase::addApplicationFont(":/resources/fonts/OpenSans-Regular.ttf");
+    QFont OpenSans("OpenSans", 10, QFont::Normal);
+    this->setFont(OpenSans);
+
+    // Initialize related modules
+    hotkey = new QHotkey(QKeySequence("ctrl+alt+Q"), true, this);
+    screenArea = new ScreenArea;
+    recognizer = new Recognizer(this);
+    translator = new Translator;
+    tray = new Tray;
+
+    // Setup connection between the modules in order of execution
+    QObject::connect(hotkey, SIGNAL(activated()), screenArea, SLOT(show()));
+    QObject::connect(screenArea, SIGNAL(selected(QPixmap)), recognizer, SLOT(start(QPixmap)));
+    QObject::connect(recognizer, SIGNAL(recognized(QString)), translator, SLOT(translate(QString)));
+    QObject::connect(translator, SIGNAL(translated(QString, QString)), tray, SLOT(showMessage(QString,QString)));
 }
